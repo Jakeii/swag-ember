746e797f206667598c17cc52ecae512db0e6bdee
Jakeii <jake@bodhi.io>
2015-10-21 17:21:55 +0100

    swagmap revision update likn

--- app/adapters/swagmap.js
+++ app/adapters/swagmap.js
@@ -3,9 +3,9 @@ import ApplicationAdapter from './application';
 export default ApplicationAdapter.extend({
   //namespace: 'api'
   urlForFindRecord() {
-    return 'https://rawgit.com/tunapanda/swagmaps/3285ac0826815078f49b67520509b045f826fcca/PythonExample/v2.0.json';
+    return 'https://rawgit.com/tunapanda/swagmaps/7a119d02fda56f1ee9539d871b642821ad270d0e/PythonExample/v2.0.json';
   },
   urlForFindAll() {
-    return 'https://rawgit.com/tunapanda/swagmaps/3285ac0826815078f49b67520509b045f826fcca/PythonExample/v2.0.json';
+    return 'https://rawgit.com/tunapanda/swagmaps/7a119d02fda56f1ee9539d871b642821ad270d0e/PythonExample/v2.0.json';
   },
 });
